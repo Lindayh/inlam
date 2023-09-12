a9d00c50e03749acc2e7bb2573c5ffceab8e7886
Linda Fogli <foglilinda@gmail.com>
2023-09-12 17:11:12 +0200

    Diagnos

--- Python_Grund2023/Diagnos_vecka37/Diagnos.py
+++ Python_Grund2023/Diagnos_vecka37/Diagnos.py
@@ -7,15 +7,14 @@ def skrivTal(tal):
     for i in range(2,tal+1,1):   #Skapa en lista med alla nummer mellan 2 och input
         numList.append(i)
 
-    while p<=tal:
+    for p in range(2,tal,1):
         for i in range(1, tal+1,1):        
             while i in numList:         #Kolla om ett nummer har inte tagits bort från lista än
                 if i%p==0 and p!=i:     #Kolla om numret är multiplikator. Man ska inte dela samma nummer med sig själv (t.ex. 3/3)
                     numList.remove(i)   #Ta bort från listan dem numren som uppfyller villkoren för att de är inte primtal
                 else:
                     break
-        p+=1
-
+    
     return numList
 
 while True:              
